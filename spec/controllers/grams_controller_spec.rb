adaf7ba94dc4f2ba4d711b614a741b256781d4c1
Teddy <tcastro@sandiego.edu>
2019-06-28 14:21:01 +0000

    TDD 11

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -17,11 +17,7 @@ RSpec.describe GramsController, type: :controller do
 
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -38,11 +34,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should succseffully create a new gram in our database" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
 
@@ -55,11 +47,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { gram: { message: '' } }
